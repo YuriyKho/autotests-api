e11b59f9fd69bbc6311b606bf2f76b414160d945
YuriyKho <yurikhokhlov@rambler.ru>
2026-06-25 00:00:25 +0300

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Test")
-print('Hello from github desktop')
+import course_service_pb2
+
+# Проверяем наличие классов
+print(dir(course_service_pb2))
+
+# Проверяем конкретные классы
+try:
+    print(course_service_pb2.GetCourseRequest)
+    print(course_service_pb2.GetCourseResponse)
+    print("Классы найдены!")
+except AttributeError as e:
+    print(f"Класс не найден: {e}")
